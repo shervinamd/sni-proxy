595a2318ddd8b5f5e6bb11cd6bb464d7a126edcb
Shervin Ahmadi <82713549+shervinamd@users.noreply.github.com>
2025-11-11 15:17:20 +0000

    Update dnsproxy entrypoint script.

--- dnsproxy-entrypoint.sh
+++ dnsproxy-entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
-sed -i "s/socks4/socks5/g; s/127\.0\.0\.1/${SOCKS_IP}/g; s/9050/${SOCKS_PORT}/g" /etc/proxychains4.conf
-/usr/bin/proxychains /service/dnsproxy --config-path=/service/config.yaml
+SOCKS_IP="${SOCKS_IP:-127.0.0.1}"
+SOCKS_PORT="${SOCKS_PORT:-1080}"
+
+ip tuntap add mode tun dev tun0
+ip addr add 192.168.100.1/24 dev tun0
+ip link set dev tun0 up
+
+iptables -t mangle -A OUTPUT ! --destination 192.168.0.0/16 -p tcp --dport 80 -j MARK --set-mark 1
+iptables -t mangle -A OUTPUT ! --destination 192.168.0.0/16 -p tcp --dport 443 -j MARK --set-mark 1
+
+echo "1 mymark" >> /etc/iproute2/rt_tables
+
+ip route add default via 192.168.100.1 dev tun0 table mymark
+ip rule add fwmark 1 table mymark
+
+/service/dnsproxy --config-path=/service/config.yaml & \ 
+/service/tun2socks -device tun0 -proxy socks5://${SOCKS_IP}:${SOCKS_PORT}
